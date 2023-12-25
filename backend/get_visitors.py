3d8edf3ce14c16dbae22bc3a0cd35948e9631a45
Ryan-Pi <68978091+Ryan-Pi@users.noreply.github.com>
2023-12-26 04:22:24 +1100

    update lambda function to return proper json response

--- backend/get_visitors.py
+++ backend/get_visitors.py
@@ -6,19 +6,42 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('test-site')
     
-    # increment visitor count by 1 and then return visitor count
-    update = table.update_item(
-        Key={
-            'counter': 'visitor',
-            'type': '1'
-        },
-        UpdateExpression="set numCount = numCount + :inc",
-        ExpressionAttributeValues={":inc": 1},
-        ReturnValues="UPDATED_NEW"
+    visitors = table.get_item(Key={
+        'counter': 'visitor'
+        }
     )
+    
+    # if visitor item is non-existent, create new item
+    if "Item" not in visitors:
+        table.put_item(
+            Item={'counter': 'visitor','numCount': 1}
+        )
+        return 1
+    else:
+    # increment visitor count by 1 and then return visitor count
+        update = table.update_item(
+            Key={
+                'counter': 'visitor'
+            },
+            UpdateExpression="set numCount = numCount + :inc",
+            ExpressionAttributeValues={":inc": 1},
+            ReturnValues="UPDATED_NEW"
+        )
     
     # extract visitor count from json response and return to requester
     # could just return this instead, but leaving it in for now
     update = update['Attributes']['numCount']
 
-    return update
+    return respond(update)
+
+def respond(visitCount):
+    object = {"numVisitors": int(visitCount)}
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": object,
+    }
+    
+    return json.dumps(response)
